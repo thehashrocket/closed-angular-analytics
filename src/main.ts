eae354aea5ad75a812b37167b5b9f5f35051bb15
YOUR NAME <jasshultz@gmail.com>
2016-07-31 15:41:43 -0600

    init commit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
+import { FIREBASE_PROVIDERS, defaultFirebase } from 'angularfire2';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  FIREBASE_PROVIDERS,
+    // Initialize Firebase app
+    defaultFirebase({
+      apiKey: "<your-key>",
+      authDomain: "<your-project-authdomain>",
+      databaseURL: "<your-database-URL>",
+      storageBucket: "<your-storage-bucket>"
+    })
+  ]);
